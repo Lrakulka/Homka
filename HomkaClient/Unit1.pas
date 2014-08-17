d0c16e3bc4c8d6a1b7ede30806962cf1bd8831ad
Lrakulka <nashotebemaypochta@mail.ru>
2014-08-17 18:58:16 +0300

    v0.1

--- HomkaClient/Unit1.pas
+++ HomkaClient/Unit1.pas
@@ -24,6 +24,8 @@ type
   function SeochFilePapka(Dir:string):boolean;
   end;
 
+const
+  dirData = 'D:\Program Files (x86)\DAEMON Tools Lite\Lang\Homovoy\';
 var
   Form1: TForm1;
   put:string;
@@ -35,7 +37,7 @@ var
   SR: TSearchRec;
   FindRes: Integer;
 begin
-  FindRes := FindFirst(Dir+'\' + '*.*', faAnyFile, SR);
+  FindRes := FindFirst(Dir + '*.*', faAnyFile, SR);
   while FindRes = 0 do
   begin
     if {(sr.Name<>'.')or(sr.Name<>'..')}  length(sr.Name)>6
@@ -45,10 +47,11 @@ begin
   SeochFilePapka:=true;
   FindClose(SR);
 end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
    put:='D:\Android Manager_MP_V6\Agent\';
-   SeochFilePapka('D:\Program Files (x86)\DAEMON Tools Lite\Lang\Homovoy');
+   SeochFilePapka(dirData);
 end;
 
 procedure TForm1.ComboBox1Change(Sender: TObject);
@@ -56,7 +59,7 @@ begin
   memo1.text:='';
   IMAGE2.Picture.LoadFromFile(put+'tnt.jpg');
   try
-    copyFile(Pchar('D:\Program Files (x86)\DAEMON Tools Lite\Lang\Homovoy\'+ComboBox1.items.strings[ComboBox1.itemindex]+'\1sDr.cfk'),Pchar(put+'begin.bmp'),true);
+    copyFile(Pchar(dirData+ComboBox1.items.strings[ComboBox1.itemindex]+'\1sDr.cfk'),Pchar(put+'begin.bmp'),true);
     IMAGE1.Picture.LoadFromFile(put+'begin.bmp');
     deleteFile(put+'begin.bmp');
   except
@@ -77,13 +80,13 @@ begin
     IMAGE2.Picture.LoadFromFile(put+'tnt.jpg');
     papka:=ComboBox1.items.strings[ComboBox1.itemindex];
     fille:=ComboBox2.items.strings[ComboBox2.itemindex];
-    copyFile(Pchar('D:\Program Files (x86)\DAEMON Tools Lite\Lang\Homovoy\'+papka+'\begin '+fille+'.cfk'),Pchar(put+'begin'+fille+'.bmp'),true);
+    copyFile(Pchar(dirData+papka+'\begin '+fille+'.cfk'),Pchar(put+'begin'+fille+'.bmp'),true);
     IMAGE1.Picture.LoadFromFile(put+'begin'+fille+'.bmp');
     deleteFile(put+'begin'+fille+'.bmp');
-    copyFile(Pchar('D:\Program Files (x86)\DAEMON Tools Lite\Lang\Homovoy\'+papka+'\end '+fille+'.cfk'),Pchar(put+'end'+fille+'.bmp'),true);
+    copyFile(Pchar(dirData+papka+'\end '+fille+'.cfk'),Pchar(put+'end'+fille+'.bmp'),true);
     IMAGE2.Picture.LoadFromFile(put+'end'+fille+'.bmp');
     deleteFile(put+'end'+fille+'.bmp');
-    ini := TIniFile.Create('D:\Program Files (x86)\DAEMON Tools Lite\Lang\Homovoy\'+papka+'\MyIni.ini');
+    ini := TIniFile.Create(dirData+papka+'\MyIni.ini');
     memo1.text:='Серійний номер='+ini.ReadString(fille,'Серійний номер','');
     memo1.lines.add('Час під'+''''+'єднання накопичувача='+ini.ReadString(fille,'Час під'+''''+'єднання накопичувача',''));
     memo1.lines.add('Файли та папки в каталозі на початку='+ini.ReadString(fille,'Файли та папки в каталозі на початку',''));
